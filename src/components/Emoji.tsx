f5bc642aaeed148fa7fc8887b7f622d96373ef7f
mjr2595 <michaeljohnraymond@gmail.com>
2023-06-22 22:50:25 -0500

    😐👍🎯

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,18 @@
+import { Text } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: string } = {
+    3: "😐",
+    4: "👍",
+    5: "🎯",
+  };
+
+  return <Text marginTop={1}>{emojiMap[rating]}</Text>;
+};
+
+export default Emoji;
