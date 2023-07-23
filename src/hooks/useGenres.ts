aa7df9b303a1734e812d220e4b3255ae3cd91d47
mjr2595 <michaeljohnraymond@gmail.com>
2023-07-23 00:03:24 -0500

    staleTime and initialData

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -12,6 +13,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
